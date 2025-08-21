8f278acd20efe501cd45d43e3b76bb66092b62b5
jaotarzan <joaovssouza59@gmail.com>
2025-08-21 15:00:13 -0300

    a

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Configuração de CORS
 const allowedOrigins = [
-  "http://192.168.0.103:5173", // seu frontend dev
+  "https://192.168.0.103:5173", // seu frontend dev
   "https://auth-tests.onrender.com/" // frontend em produção
 ];
 
